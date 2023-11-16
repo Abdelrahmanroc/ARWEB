bc968e20d5510fa21b6b15eaab180873fb019658
Abdelrahmanroc <90619774+Abdelrahmanroc@users.noreply.github.com>
2023-11-16 10:38:33 +0100

    checkout.php

--- checkout.php
+++ checkout.php
@@ -1,18 +1,20 @@
 <?php 
+// Inclusie van de databaseverbinding en start van de sessie.
  include 'components/connection.php';
+ // Controleert of de gebruiker is ingelogd en haalt de gebruikers-ID op.
  session_start();
  if (isset($_SESSION['user_id'])) {
 		$user_id = $_SESSION['user_id'];
 	}else{
 		$user_id = '';
 	}
-
+// Uitlogfunctionaliteit: vernietigt de sessie bij het indienen van het uitlogformulier en leidt door naar het inlogscherm.
 	if (isset($_POST['logout'])) {
 		session_destroy();
 		header("location: login.php");
 	}
 	if (isset($_POST['place_order'])) {
-
+// Verzamelen van factuurgegevens uit het POST-formulier en uitvoeren van vereiste validatie en filtering.
 		$name = $_POST['name'];
 		$name = filter_var($name, FILTER_SANITIZE_STRING);
 		$number = $_POST['number'];
@@ -25,15 +27,16 @@
 		$address_type = filter_var($address_type, FILTER_SANITIZE_STRING);
 		$method = $_POST['method'];
 		$method = filter_var($method, FILTER_SANITIZE_STRING);
-
+		// Controleren of er items in de winkelwagen zijn.
 		$varify_cart = $conn->prepare("SELECT * FROM `cart` WHERE user_id=?");
 		$varify_cart->execute([$user_id]);
-
+		// Verwerken van bestelling met betrekking tot een specifiek product.
 		if (isset($_GET['get_id'])) {
 			$get_product = $conn->prepare("SELECT * FROM `products` WHERE id=? LIMIT 1");
 			$get_product->execute([$_GET['get_id']]);
 			if ($get_product->rowCount() > 0) {
 				while($fetch_p = $get_product->fetch(PDO::FETCH_ASSOC)){
+					// Invoegen van de bestelling in de database en doorverwijzen naar de orderpagina.
 					$insert_order = $conn->prepare("INSERT INTO `orders`(id, user_id, name, number, email, address, address_type, method, product_id, price, qty) VALUES(?,?,?,?,?,?,?,?,?,?,?)");
 			        $insert_order->execute([unique_id(), $user_id, $name, $number, $email, $address, $address_type, $method, $fetch_p['id'], $fetch_p['price'], 1]);
 			            header('location:order.php');
@@ -44,12 +47,14 @@
 				$warning_msg[] = 'somthing went wrong';
 			}
 		}elseif ($varify_cart->rowCount()>0) {
+			// Verwerken van de bestelling met betrekking tot de items in de winkelwagen.	
 			while($f_cart = $varify_cart->fetch(PDO::FETCH_ASSOC)){
 				$insert_order = $conn->prepare("INSERT INTO `orders`(id, user_id, name, number, email, address, address_type, method, product_id, price, qty) VALUES(?,?,?,?,?,?,?,?,?,?,?)");
 			        $insert_order->execute([unique_id(), $user_id, $name, $number, $email, $address, $address_type, $method, $f_cart['product_id'], $f_cart['price'], $f_cart['qty']]);
 			            header('location:order.php');
 			}
 			if ($insert_order) {
+				// Leegmaken van de winkelwagen na het plaatsen van de bestelling.
 				$delete_cart_id = $conn->prepare("DELETE FROM `cart` WHERE user_id = ?");
 				$delete_cart_id->execute([$user_id]);
 				header('location: order.php');
@@ -75,6 +80,7 @@
 </head>
 <body>
 	<?php include 'components/header.php'; ?>
+	<!-- Hoofdinhoud van de pagina, inclusief banner, navigatie en besteloverzicht. -->
 	<div class="main">
 		<div class="banner">
 			<h1>checkout summary</h1>
@@ -86,8 +92,8 @@
 			<div class="title">
 				<img src="img/download.png" class="logo">
 				<h1>checkout summary</h1>
-				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto dolorum deserunt minus veniam
-                    tenetur
+				<!-- <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto dolorum deserunt minus veniam
+                    tenetur -->
                 </p>
             </div>
                 <div class="row">
@@ -114,7 +120,7 @@
                 						<option value="credit or debit card">credit or debit card</option>
                 						<option value="net banking">net banking</option>
                 						<option value="UPI or RuPay">UPI or RuPay</option>
-                						<option value="paytm">paytm</option>
+                						<option value="paytm">PayPal</option>
                 					</select>
                 				</div>
                 				<div class="input-field">
@@ -155,6 +161,7 @@
                 		<h3>my bag</h3>
                 		<div class="box-container">
                 			<?php 
+							 // Controleer of een specifieke product-ID is ingesteld in de URL 
                 				$grand_total=0;
                 				if (isset($_GET['get_id'])) {
                 					$select_get = $conn->prepare("SELECT * FROM `products` WHERE id=?");
@@ -162,7 +169,7 @@
                 					while($fetch_get = $select_get->fetch(PDO::FETCH_ASSOC)){
                 						$sub_total = $fetch_get['price'];
                 						$grand_total+=$sub_total;
-                					
+                					// Controleer of het product is gevonden
                 			?>
                 			<div class="flex">
                 				<img src="image/<?=$fetch_get['image']; ?>" class="image">
@@ -174,15 +181,20 @@
                 			<?php 
                 					}
                 				}else{
+									// Als er geen specifieke product-ID is ingesteld, toon items uit de winkelwagen van de gebruiker
                 					$select_cart = $conn->prepare("SELECT * FROM `cart` WHERE user_id=?");
                 					$select_cart->execute([$user_id]);
+									 // Controleer of er items aanwezig zijn in de winkelwagen van de gebruiker
                 					if ($select_cart->rowCount()>0) {
                 						while($fetch_cart = $select_cart->fetch(PDO::FETCH_ASSOC)){
+											// Haal details op van het product in de winkelwagen
                 							$select_products=$conn->prepare("SELECT * FROM `products` WHERE id=?");
                 							$select_products->execute([$fetch_cart['product_id']]);
                 							$fetch_product = $select_products->fetch(PDO::FETCH_ASSOC);
+											 // Bereken de subtotaal voor elk item in de winkelwagen
                 							$sub_total= ($fetch_cart['qty'] * $fetch_product['price']);
                 							$grand_total += $sub_total;
+											 // Toon informatie over het winkelwagenitem in een flexibele container
                 						
                 			?>
                 			<div class="flex">
@@ -195,6 +207,7 @@
                 			<?php 
                 						}
                 					}else{
+										 // Toon een bericht als de winkelwagen van de gebruiker leeg is
                 						echo '<p class="empty">your cart is empty</p>';
                 					}
                 				}
